72e8b34448d53db7b4c133632253cb51756c38a5
Roxanne Bei <73452103+roxannebei@users.noreply.github.com>
2020-10-29 14:23:00 +0800

    week01class02 homework extra

---
+++ Week 01 - Class 02/Homework/rb347_week01class02_codeSketch01/src/Ball.cpp
@@ -0,0 +1,8 @@
+//
+//  Ball.cpp
+//  rb347_week01class02_codeSketch01
+//
+//  Created by Rongqi Bei on 2020/10/28.
+//
+
+#include "Ball.hpp"
---
+++ Week 01 - Class 02/Homework/rb347_week01class02_codeSketch01/src/Ball.h
@@ -0,0 +1,13 @@
+//
+//  Ball.hpp
+//  rb347_week01class02_codeSketch01
+//
+//  Created by Rongqi Bei on 2020/10/28.
+//
+
+#ifndef Ball_hpp
+#define Ball_hpp
+
+#include <stdio.h>
+
+#endif /* Ball_hpp */
